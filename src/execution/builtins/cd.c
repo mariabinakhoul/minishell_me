5e0ac06a6987b47bbe7f6e41cd9f245e1a5cb2bf
Nehme Haber <nehmehabr19@gmail.com>
2025-04-01 22:57:33 +0300

    FInnaly fixed the segfault

--- src/execution/builtins/cd.c
+++ src/execution/builtins/cd.c
@@ -3,16 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mabi-nak <mabi-nak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nhaber <nhaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 19:35:53 by mabi-nak          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/04/01 12:40:52 by nhaber           ###   ########.fr       */
-=======
-/*   Updated: 2025/04/01 02:36:36 by mabi-nak         ###   ########.fr       */
->>>>>>> refs/remotes/origin/main
+/*   Updated: 2025/04/01 22:34:54 by nhaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "../../../includes/minishell.h"
 
@@ -40,20 +37,20 @@ static char *retrieve_env_path(t_ast *cmd, char **envp, char *key, const char *e
     return NULL;
 }
 
-// static char *get_home_or_oldpwd(t_ast *cmd, char **envp)
-// {
-//     if (!cmd->params[1])
-//         return retrieve_env_path(cmd, envp, "HOME", "cd: HOME not set", false);
+static char *get_home_or_oldpwd(t_ast *cmd, char **envp)
+{
+    if (!cmd->params[1])
+        return retrieve_env_path(cmd, envp, "HOME", "cd: HOME not set", false);
 
-//     if (ft_strcmp(cmd->params[1], "-") == 0)
-//         return retrieve_env_path(cmd, envp, "OLDPWD", "cd: OLDPWD not set", true);
-//     // if (ft_strcmp(cmd->params[1], "--") == 0)
-//     // {
-//     //     ft_cd(cmd,envp);
-//     //     printf("%s", ft_get_oldpwd());
-//     // }
-//     return (char *)cmd->params[1];
-// }
+    if (ft_strcmp(cmd->params[1], "-") == 0)
+        return retrieve_env_path(cmd, envp, "OLDPWD", "cd: OLDPWD not set", true);
+    // if (ft_strcmp(cmd->params[1], "--") == 0)
+    // {
+    //     ft_cd(cmd,envp);
+    //     printf("%s", ft_get_oldpwd());
+    // }
+    return (char *)cmd->params[1];
+}
 
 void update_env(t_ast *path)
 {
@@ -105,35 +102,26 @@ void ft_setenv(t_ast *cmd_path)
 
 
 void ft_cd(t_ast *cmd, char **envp) {
-    char *path = NULL;
+    char *path;
     char *old_pwd;
-
+    // char **envp_ptr = envp;
+    // printf("TEST123  :  %s",*envp_ptr);
+    // printf("\n=== FT_CD ===\n");
+    // printf("Params: ");
     if (cmd->params) {
         for (int i = 0; cmd->params[i]; i++) {
             printf("[%s] ", cmd->params[i]);
         }
     }
     printf("\n");
-
-        fprintf(stderr, "bash: cd: too many arguments\n");
     if (cmd->params && cmd->params[1] && cmd->params[2]) {
+        fprintf(stderr, "bash: cd: too many arguments\n");
         return;
     }
-
-    // if (cmd->params[1] && strcmp(cmd->params[1], "~") == 0)
-    //     cmd->params[1] = NULL;  // Treat `~` as no argument
-
-    // // Handle `cd -`
-    // if (cmd->params[1] && ft_strcmp(cmd->params[1], "-") == 0) {
-    //     path = retrieve_env_path(cmd, envp, "OLDPWD", "cd: OLDPWD not set", true);
+    path = get_home_or_oldpwd(cmd, envp);
     if (!path)
-        return;  // If OLDPWD is not set, return early
-    printf("%s\n", path);  // Print the new directory (like Bash)
-    // } else {
-    //     path = get_home_or_oldpwd(cmd, envp);
-    // }
-    if (!path) {
-        fprintf(stderr, "cd: could not determine target directory\n");
+    {
+        ft_putstr_fd("cd: could not determine target directory\n", 1);
         return;
     }
     old_pwd = getcwd(NULL, 0);
@@ -142,13 +130,14 @@ void ft_cd(t_ast *cmd, char **envp) {
         return;
     }
     printf("Attempting to change to: %s\n", path);
-    if (chdir(path) != 0) {
+    if (chdir(path) != 0)
+    {
+        printf("hello1");
         perror("cd");
         free(old_pwd);
         return;
     }
+    printf("%s\n",old_pwd);
     update_env(cmd);
     free(old_pwd);
 }
-
-
