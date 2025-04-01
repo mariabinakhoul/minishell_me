6ab74ea608ad9e561e5b644d23950cf536613f47
Nehme Haber <nehmehabr19@gmail.com>
2025-04-02 00:04:11 +0300

    did some changes and added comments.

--- src/execution/builtins/cd.c
+++ src/execution/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: nhaber <nhaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 19:35:53 by mabi-nak          #+#    #+#             */
-/*   Updated: 2025/04/01 22:34:54 by nhaber           ###   ########.fr       */
+/*   Updated: 2025/04/02 00:03:20 by nhaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static char *retrieve_env_path(t_ast *cmd, char **envp, char *key, const char *e
 
     if (!cmd || !envp || !key)
         return NULL;
-    printf(" hello :  %s\n", envp[i]);
+    // printf(" hello :  %s\n", envp[i]);
     while (envp[i])
     {
         if (ft_strncmp(envp[i], key, ft_strlen(key)) == 0 && envp[i][ft_strlen(key)] == '=')
@@ -43,7 +43,10 @@ static char *get_home_or_oldpwd(t_ast *cmd, char **envp)
         return retrieve_env_path(cmd, envp, "HOME", "cd: HOME not set", false);
 
     if (ft_strcmp(cmd->params[1], "-") == 0)
+    {
         return retrieve_env_path(cmd, envp, "OLDPWD", "cd: OLDPWD not set", true);
+        // update_env(cmd);
+    }
     // if (ft_strcmp(cmd->params[1], "--") == 0)
     // {
     //     ft_cd(cmd,envp);
@@ -62,6 +65,8 @@ void update_env(t_ast *path)
 //     // int i = 0;
 //     // char *new_dir = strcat("/", dir);
 //     // char *env = envp[i];
+//     if (strcmp(cmd_path->params[1], "..") == 0)
+//         return ;
 //     if (cmd_path->params[1])
 //     {
 //         char *path;
@@ -77,6 +82,14 @@ void update_env(t_ast *path)
 //     //     env  = envp[i];
 //     // }
 // }
+
+/*
+    *Both setenvs are not working but the one below is completely wrong.
+
+    *if this is fixed the "cd -" case is automatically fixed.
+    
+    *Dont fix it so git does not cause conflict!!!!!! (If Maria is reading this). 
+ */
 
 void ft_setenv(t_ast *cmd_path)
 {
@@ -95,49 +108,52 @@ void ft_setenv(t_ast *cmd_path)
         // Now update your environment variable or internal PWD accordingly,
         // for example with setenv("PWD", new_path, 1);
         setenv("PWD", new_path, 1);
-
         free(new_path);
     }
 }
 
 
 void ft_cd(t_ast *cmd, char **envp) {
-    char *path;
+    char *path = NULL;
     char *old_pwd;
-    // char **envp_ptr = envp;
-    // printf("TEST123  :  %s",*envp_ptr);
-    // printf("\n=== FT_CD ===\n");
-    // printf("Params: ");
-    if (cmd->params) {
-        for (int i = 0; cmd->params[i]; i++) {
-            printf("[%s] ", cmd->params[i]);
-        }
-    }
-    printf("\n");
+
     if (cmd->params && cmd->params[1] && cmd->params[2]) {
         fprintf(stderr, "bash: cd: too many arguments\n");
         return;
     }
-    path = get_home_or_oldpwd(cmd, envp);
-    if (!path)
-    {
-        ft_putstr_fd("cd: could not determine target directory\n", 1);
+
+    if (cmd->params[1] && strcmp(cmd->params[1], "~") == 0)
+        cmd->params[1] = NULL;
+
+    //  `cd -`
+    if (cmd->params[1] && ft_strcmp(cmd->params[1], "-") == 0) {
+        path = retrieve_env_path(cmd, envp, "OLDPWD", "cd: OLDPWD not set", true);
+    if (!path) 
+        return; 
+    printf("%s\n", path);  // Print the new directory (like Bash)
+    } else {
+        path = get_home_or_oldpwd(cmd, envp);
+    }
+
+    if (!path) {
+        fprintf(stderr, "cd: could not determine target directory\n");
         return;
     }
+
     old_pwd = getcwd(NULL, 0);
     if (!old_pwd) {
         perror("cd: getcwd");
         return;
     }
+
     printf("Attempting to change to: %s\n", path);
-    if (chdir(path) != 0)
-    {
-        printf("hello1");
+
+    if (chdir(path) != 0) {
         perror("cd");
         free(old_pwd);
         return;
     }
-    printf("%s\n",old_pwd);
-    update_env(cmd);
+
+    update_env(cmd);  // Ensure OLDPWD and PWD are updated
     free(old_pwd);
 }
